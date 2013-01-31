7216e47629449b844876c5da2cc38ed1a1be4c41
Tommy Bishop <bishop.thomas@gmail.com>
2013-01-30 16:48:15 -0800

    add emailer class

--- lib/aws-alert-monitor.rb
+++ lib/aws-alert-monitor.rb
@@ -1,6 +1,7 @@
 require "aws-alert-monitor/alert"
 require "aws-alert-monitor/aws"
 require "aws-alert-monitor/config"
+require "aws-alert-monitor/emailer"
 require "aws-alert-monitor/logger"
 require "aws-alert-monitor/parser"
 require "aws-alert-monitor/version"
